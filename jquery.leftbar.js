b053c8aa2cb553db30f509205f1a858345cf7edf
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-02-01 19:42:07 +0100

    Estructura básica de plugin

--- jquery.leftbar.js
+++ jquery.leftbar.js
@@ -3,3 +3,18 @@
  * @copyright Copyright © 2017 Raúl Caro Pastorino
  * @license https://wwww.gnu.org/licenses/gpl.txt
 **/
+
+$.fn.leftbar = function(opciones) {
+        var configuracion = {
+            dato1: '',
+        };
+
+        $.extend(configuracion, opciones);
+
+        this.each(function() {
+
+        });
+
+        return this;
+    };
+})(jQuery);
