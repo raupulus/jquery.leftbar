b8c34802d4e29162844d261f871f8eda619aaafc
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-02-04 22:30:26 +0100

    Planteo estructura de plugin

--- jquery.leftbar.js
+++ jquery.leftbar.js
@@ -3,3 +3,19 @@
  * @copyright Copyright © 2017 Raúl Caro Pastorino
  * @license https://wwww.gnu.org/licenses/gpl.txt
 **/
+
+(function($) {
+    $.fn.leftbar = function() {
+        var configuracion = {
+            dato1: '',
+        };
+
+        $.extend(configuracion, opciones);
+
+        this.each(function() {
+
+        });
+
+        return this;
+    };
+})(jQuery);
