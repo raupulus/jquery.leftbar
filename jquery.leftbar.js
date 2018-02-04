7917b5f7ddd8432f286c5bf7f760d6b42b767a97
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-02-04 22:44:22 +0100

    Avanzo en el planteamiento de la estructura

--- jquery.leftbar.js
+++ jquery.leftbar.js
@@ -5,17 +5,31 @@
 **/
 
 (function($) {
-    $.fn.leftbar = function() {
-        var configuracion = {
-            dato1: '',
+    /**
+     * Recibe un objeto con la configuración para personalizar el plugin y
+     * un array con la información de cada menú que contendrá la barra lateral.
+     * @param  {Objetct} options  Objeto de configuración
+     * @param  {Array}   entradas Array que contiene un array por cada entrada
+     */
+    $.leftbar = function(options, entradas) {
+        var conf = {
+            background1: '',
+            background2: '',
+            color: '',
+            border: '',
         };
 
-        $.extend(configuracion, opciones);
+        $.extend(conf, options);
 
-        this.each(function() {
+        $('body').append('<div id="cajaDesplegableIzq"></div>');
 
+        // Por cada entrada añade un elemento al menú
+        entradas.each(function() {
+            var agregar = '' +
+                '<div id="???">' +
+                    '<img src="???" />' +
+                    '<div id="???">??titulo??</div>' +
+                '</div>'
         });
-
-        return this;
     };
 })(jQuery);
